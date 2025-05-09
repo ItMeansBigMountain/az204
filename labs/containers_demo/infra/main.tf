7c0e8b95ee40f8c0a402768d095b9df2da198c17
Affan <affan.fareed@gmail.com>
2025-05-09 07:42:45 -0700

    Flattened embedded repo, now part of AZ204 project

--- labs/containers_demo/infra/main.tf
+++ labs/containers_demo/infra/main.tf
@@ -13,6 +13,14 @@ provider "azurerm" {
   subscription_id = var.subscription_id
 }
 
+resource "azurerm_container_registry" "trapistan_acr" {
+  name                = "trapistanacr"
+  resource_group_name = "az204"
+  location            = "Central US"
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
 resource "azurerm_container_group" "trapistan_aci" {
   name                = "trapistan-aci"
   location            = "Central US"
@@ -20,8 +28,15 @@ resource "azurerm_container_group" "trapistan_aci" {
   os_type             = "Linux"
   restart_policy      = "OnFailure"
 
-  ip_address_type     = "Public"
-  dns_name_label      = "trapistan-aci"
+  ip_address_type = "Public"
+  dns_name_label  = "trapistan-aci"
+
+  # image_registry_credential {
+  #   server   = azurerm_container_registry.trapistan_acr.login_server
+  #   username = azurerm_container_registry.trapistan_acr.admin_username
+  #   password = azurerm_container_registry.trapistan_acr.admin_password
+  # }
+
 
   container {
     name   = "trapistan-aci"
